ce9744b59706c66904bc832c1e8579bf9edfff70
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-18 00:45:28 +0530

    [ft_setup_match] - create GameManager to manage user websocket connections

---
+++ backend_v1/src/GameManager.ts
@@ -0,0 +1,22 @@
+import { WebSocket } from 'ws';
+
+export class GameManager {
+  private games: Game[];
+
+  constructor() {
+    this.games = [];
+  }
+
+  addUser(user: WebSocket) {
+    
+  }
+
+  removeUser(user: WebSocket) {
+    
+  }
+
+  private handleMessage(){
+
+  }
+  
+}
