d2e9943f7a1fc331d5e28b35fff4a0326e9df175
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-18 01:58:32 +0530

    [ft_setup_match] - add validations and make moves on the board

--- backend_v1/src/Games.ts
+++ backend_v1/src/Games.ts
@@ -1,30 +1,68 @@
+import { Chess } from 'chess.js';
 import { WebSocket } from 'ws';
+import { GAME_OVER, MOVE } from './messages';
 
 export class Game {
     public player1UserId: WebSocket;
-    public player2UserId: WebSocket | null;
-    public board: string;
+    public player2UserId: WebSocket;
+    public board: Chess;
     private startTime = new Date(Date.now());
 
-    constructor(player1UserId: WebSocket, player2UserId: WebSocket | null, startTime?: Date) {
+    constructor(player1UserId: WebSocket, player2UserId: WebSocket, startTime?: Date) {
         this.player1UserId = player1UserId;
         this.player2UserId = player2UserId;
-        this.board = "";
+        this.board = new Chess();
         if(startTime){
             this.startTime = startTime;
         }
     }
 
-    makeMove(socket: WebSocket, move: string) {
+    makeMove(socket: WebSocket, move: {
+        from: string;
+        to: string;
+    }) {
         // Validations:-
             // Which users move ?
+            if (this.board.moves.length % 2 === 0 && socket !== this.player1UserId) {
+                return;
+            }
+        
+            if (this.board.moves.length % 2 === 1 && socket !== this.player2UserId) {
+                return;
+            }
+
             // Is it a valid move ?
         //THEN
             // Update the board
             // Push the move
-            // Check if the game is over
-        // Send updated board to both the players
-        
+            try{
+                this.board.move(move);
+            }catch(e){
+                return; // Chess.js library will throw error if its an invalid move.
+            }
 
+            // Check if the game is over
+            if (this.board.isGameOver()) {
+                // Send game_over msg to both players
+                this.player1UserId.emit(JSON.stringify({
+                    type: GAME_OVER,
+                    payload: {
+                        winner: this.board.turn() === 'b' ? 'WHITE_WINS': 'BLACK_WINS'
+                    }
+                }));
+              }
+
+        // Send updated board to both the players
+        if(this.board.moves.length % 2 === 0){
+            this.player2UserId.emit(JSON.stringify({
+                type: MOVE,
+                payload: move
+            }));
+        }else{
+            this.player1UserId.emit(JSON.stringify({
+                type: MOVE,
+                payload: move
+            }));
+        }
     }
 }
