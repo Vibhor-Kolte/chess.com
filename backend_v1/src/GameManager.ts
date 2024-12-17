e11310184fa3b0826428096bf86745b507e18d52
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-18 00:55:52 +0530

    [ft_setup_match] - add/remove user logic

--- backend_v1/src/GameManager.ts
+++ backend_v1/src/GameManager.ts
@@ -2,21 +2,26 @@ import { WebSocket } from 'ws';
 
 export class GameManager {
   private games: Game[];
+  private pendingUser: WebSocket | null;
+  private users: WebSocket[];
 
   constructor() {
     this.games = [];
+    this.pendingUser = null;
+    this.users = [];
   }
 
   addUser(user: WebSocket) {
-    
+    this.users.push(user);
   }
 
-  removeUser(user: WebSocket) {
-    
+  removeUser(socket: WebSocket) {
+    this.users = this.users.filter((user) => user !== socket);
+    // Stop the game here as user left or have reconnect logic
   }
 
   private handleMessage(){
 
   }
-  
+
 }
