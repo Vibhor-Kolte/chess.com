176efa08d36dbc4e73766365514486dedc346b00
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-18 16:04:25 +0530

    [ft_build_frontend] - add basic routes

--- frontend_v1/src/App.tsx
+++ frontend_v1/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Landing } from './screens/Landing';
+import { Game } from './screens/Game';
 
 function App() {
-
   return (
     <>
-      <button className='bg-red-200'>
-        Join Room
-      </button>
+      <BrowserRouter basename='/app'>
+        <Routes>
+          <Route path='/' element={<Landing />}></Route>
+          <Route path='/game' element={<Game />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
