0c7aa2967f159afa4db93a00ce1e640a34726f20
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-21 21:20:46 +0530

    [ft_build_frontend] - ui improvements

--- frontend_v1/src/screens/Landing.tsx
+++ frontend_v1/src/screens/Landing.tsx
@@ -2,17 +2,17 @@ import chessBoard from '../assets/chess-board.png';
 
 export const Landing = () => {
   return (
-    <div>
-      <div className="pt-8">
+    <div className='flex justify-center'>
+      <div className="pt-8 max-w-screen-lg">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
           <div className='flex justify-center'>
             <img src={chessBoard} className='max-w-96'/>
           </div>
 
-          <div>
-            <h1 className="text-4xl font-bold text-white">Play Chess Online on #2 Site!</h1>
-            <div className="mt-4">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Play Now</button>
+          <div className='pt-8'>
+            <h1 className="text-4xl font-bold text-white flex justify-center">Play Chess Online on #2 Site!</h1>
+            <div className="mt-6 flex justify-center">
+              <button className="bg-lime-600 hover:bg-blue-600 text-white font-bold py-4 px-8 rounded">Play Now</button>
             </div>
           </div>
           </div>
