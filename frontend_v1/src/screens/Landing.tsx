1917ab218c04bde31ed070dee68fc77ed9569cd7
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-21 21:23:39 +0530

    [ft_build_frontend] - navigate to game screen when play button pressed

--- frontend_v1/src/screens/Landing.tsx
+++ frontend_v1/src/screens/Landing.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import chessBoard from '../assets/chess-board.png';
 
 export const Landing = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='flex justify-center'>
       <div className="pt-8 max-w-screen-lg">
@@ -12,7 +15,12 @@ export const Landing = () => {
           <div className='pt-8'>
             <h1 className="text-4xl font-bold text-white flex justify-center">Play Chess Online on #2 Site!</h1>
             <div className="mt-6 flex justify-center">
-              <button className="bg-lime-600 hover:bg-blue-600 text-white font-bold py-4 px-8 rounded">Play Now</button>
+              <button 
+                className="bg-lime-600 hover:bg-blue-600 text-white font-bold py-4 px-8 rounded"
+                onClick={() => navigate('/game')}
+              >
+                Play Now
+              </button>
             </div>
           </div>
           </div>
