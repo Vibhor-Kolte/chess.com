f46f499e9f0aa7b69b1fe9074488d6bb3fdb6324
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-21 21:58:02 +0530

    [ft_build_frontend] - design game area

---
+++ frontend_v1/src/components/ChessBoard.tsx
@@ -0,0 +1,5 @@
+import chessBoard from '../assets/chess-board.png';
+
+export const ChessBoard = () => {
+    return <img src={chessBoard} />
+}
--- frontend_v1/src/screens/Game.tsx
+++ frontend_v1/src/screens/Game.tsx
@@ -1,3 +1,64 @@
+import { useEffect } from "react";
+import { ChessBoard } from "../components/ChessBoard"
+import { useSocket } from "../hooks/useSocket";
+
+export const INIT_GAME = 'init_game';
+export const MOVE = 'move';
+export const GAME_OVER = 'game_over';
+
 export const Game = () => {
-    return <div>Game Page</div>
+    const socket = useSocket();
+
+    useEffect(() => {
+        if (!socket) {
+            return;
+        }
+
+        socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            console.log(message);
+
+            switch (message.type) {
+                case INIT_GAME:
+                    console.log('Game initialized');
+                    break;
+                case MOVE:
+                    console.log('Move made');
+                    break;
+                case GAME_OVER:
+                    console.log('Game over');
+                    break;
+                default:
+                    console.log('Unknown message type');
+            }
+        }
+    }, [socket]);
+
+    if (!socket) {
+        return <div className="text-white font-bold">Connecting...</div>
+    }
+
+    return <div>
+        <div className='flex justify-center'>
+            <div className="pt-8 max-w-screen-lg">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                    <div className='flex justify-center'>
+                        <ChessBoard/>
+                    </div>
+                    <div className='pt-8'>
+                        <div className="mt-6 flex justify-center">
+                            <button 
+                                className="bg-lime-600 hover:bg-blue-600 text-white font-bold py-4 px-48 rounded"
+                                onClick={() => {
+                                    socket.send(JSON.stringify({ type: INIT_GAME }));
+                                }}
+                            >
+                                Play
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
