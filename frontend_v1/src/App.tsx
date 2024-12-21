27f0e0e406ca4e7d8528c77bd1591629ff8737d7
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-21 21:10:51 +0530

    [ft_build_frontend] - change background color

--- frontend_v1/src/App.tsx
+++ frontend_v1/src/App.tsx
@@ -5,14 +5,14 @@ import { Game } from './screens/Game';
 
 function App() {
   return (
-    <>
+    <div className='h-screen bg-stone-600'>
       <BrowserRouter basename='/app/v1'>
         <Routes>
           <Route path='/' element={<Landing />}></Route>
           <Route path='/game' element={<Game />}></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
--- frontend_v1/src/screens/Landing.tsx
+++ frontend_v1/src/screens/Landing.tsx
@@ -3,14 +3,14 @@ import chessBoard from '../assets/chess-board.png';
 export const Landing = () => {
   return (
     <div>
-      <div className="mt-2">
+      <div className="pt-8">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-          <div>
-            <img src={chessBoard} />
+          <div className='flex justify-center'>
+            <img src={chessBoard} className='max-w-96'/>
           </div>
 
           <div>
-            <h1 className="text-4xl font-bold">Play Chess Online on #2 Site!</h1>
+            <h1 className="text-4xl font-bold text-white">Play Chess Online on #2 Site!</h1>
             <div className="mt-4">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Play Now</button>
             </div>
